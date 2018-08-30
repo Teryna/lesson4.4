ff915054de8a55293cf42a09d7e538295ef5dcf9
Jekaterina <misstery@inbox.lv>
2018-08-30 14:11:26 +0300

    Some changes

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php 
 error_reporting(E_ALL);
-$pdo = new PDO("mysql:host=localhost;dbname=db;charset=utf8", "root", "");
+$pdo = new PDO("mysql:host=localhost;dbname=JLoseva;charset=utf8", "JLoseva", "neto1801");
 if (!$pdo)
     {
         die('Невозможно подключиться');
